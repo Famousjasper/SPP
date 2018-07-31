4fe6d1fd95573d708ed7120863e9e964befc87fa
Joel Åman <Schoelxz@gmail.com>
2018-07-31 18:31:28 +0200

    Testing out code for first time.
    Changed all spaces to tabs instead.
    Added visual studio comments (#%% comment) to fold code.
    Changed argument code to take 2 default arguments (i think?)
    Added 1 extra word to find inside reserved (do ; gör)
    Added print to end of code (testing purposes)

--- interp.py
+++ interp.py
@@ -1,61 +1,57 @@
+#%% Imports
+
 import argparse
+
+#%% Code
 
 parser= argparse.ArgumentParser()
 parser.add_argument("infile")
 parser.add_argument("outfile")
-args = parser.parse_args()
+args = parser.parse_args(['input.cpp', 'output.cpp'])
 infile = args.infile
 outfile = args.outfile
 
-reserved={'if': 'om', 'for': 'för', 'int': 'hel', 'bool': 'bool', 'while': 'medans', 'float': 'flyt', 'false': 'falskt',
-          'true': 'sant', 'return': 'returnera', 'try': 'försök', 'catch': 'fånga', 'throw': 'kasta', 'this': 'detta',
-          'break': 'bryt', 'void': 'tom', 'unsigned': 'osignerad', 'char': 'karr', 'short':'kort'}
+reserved={	'if': 'om', 'for': 'för', 'int': 'hel', 'bool': 'bool', 'while': 'medans', 'float': 'flyt', 'false': 'falskt',
+			'true': 'sant', 'return': 'returnera', 'try': 'försök', 'catch': 'fånga', 'throw': 'kasta', 'this': 'detta',
+			'break': 'bryt', 'void': 'tom', 'unsigned': 'osignerad', 'char': 'karr', 'short':'kort', 'do':'gör'}
 
 def parse(line):
-    inString = False
-    done = ""
-    word = ""
-    cand = ""
-    for c in line:
-        if inString and c == "\"":
-            inString = False
-        elif c == "\"":
-            inString = True
-        if inString:
-            done += c
-        elif not inString and c.isalpha():
-            word+=c
-            if(len(cand)>0):
-                cand = ""
-            try:
-                cand = reserved[word]
-            except:
-                pass
-        elif not inString and not c.isalpha():
-            if(len(cand)>0):
-                done += cand
-                cand = ""
-            else:
-                done += word
-            word = ""
-            done += c 
-        
-    return done
+	inString = False
+	done = ""
+	word = ""
+	cand = ""
+	for c in line:
+		if inString and c == "\"":
+			inString = False
+		elif c == "\"":
+			inString = True
+		if inString:
+			done += c
+		elif not inString and c.isalpha():
+			word+=c
+		if(len(cand)>0):
+			cand = ""
+			try:
+				cand = reserved[word]
+			except:
+				pass
+		elif not inString and not c.isalpha():
+			if(len(cand)>0):
+				done += cand
+				cand = ""
+			else:
+				done += word
+			word = ""
+			done += c 
+	return done
 
 lines = []
 with open(infile, 'r') as file:
-    for line in file:
-        lines.append(parse(line))
+	for line in file:
+		lines.append(parse(line))
 
 with open(outfile, 'w') as file:
-    for line in lines:
-        file.write(line)
+	for line in lines:
+		file.write(line)
 
-
-
-            
-            
-                
-
-
-
+print("done!");
